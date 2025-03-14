5d09abf660d1150b888c946cc19cde8729dc64ee
MatFragg <ethan.aliaga@gmail.com>
2025-03-14 01:05:56 -0500

    Meeti  Version 1.0.1

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,6 +75,7 @@ const create_account = async (req,res) => {
     const expressErrors = validationResult(req);
 
     try {
+        user.id = uuidv4();
         await Users.create(user);
 
         // Generate confirmation URL
