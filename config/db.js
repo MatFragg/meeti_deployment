41c784cbb9c10306f8828b2d8b148ed3440a7f2c
MatFragg <ethan.aliaga@gmail.com>
2025-03-31 22:46:08 -0500

    SSL config commit

--- config/db.js
+++ config/db.js
@@ -5,7 +5,13 @@ dotenv.config({path: '.env'})
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
     host:process.env.DB_HOST,
     port:process.env.DB_PORT,
-    dialect: 'postgres',
+    dialect: 'postgres',dialectOptions: {
+        ssl: {
+          require: true, // ← Esto obliga a usar SSL
+          rejectUnauthorized: false, // ← Evita problemas con certificados no verificados
+        },
+      },
+    logging: false,
     pool :{
         max:5,
         min:0,
